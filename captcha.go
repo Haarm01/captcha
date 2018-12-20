004ada89dd6d6b9ed69f56583271c5d55db04650
DESKTOP-VSIOK4A\Nok <omerta@outlook.co.th>
2018-12-20 16:41:14 +0700

    refactor hardcode to for loop arrays operand2

--- captcha.go
+++ captcha.go
@@ -1,27 +1,37 @@
 package main
 
-import "strconv"
-
 func main() {
 	// Captcha(1, 1, 1, 1)
 }
 
 func Captcha(pattern, operand1, operator, operand2 int) string {
 
+	// patTern := [2]string{"1", "2"}
+	// opeRand1 := [5]string{"1", "2", "3", "4", "5"}
+	// opeRator := [3]string{"+", "-", "*"}
+	opeRand2 := [5]string{"one", "two", "three", "four", "five"}
 	result := ""
-	if operand2 == 1 {
-		return "1+one"
-	} else if operand2 == 2 {
-		return "1+two"
-	} else if operand2 == 3 {
-		return "1+three"
-	} else if operand2 == 4 {
-		return "1+four"
-	} else if operand2 == 5 {
-		return "1+five"
-	} else {
-		result = strconv.Itoa(operand2)
-		return "Error " + result
+
+	for i, v := range opeRand2 {
+		if i+1 == operand2 {
+			result = "1+" + v
+		}
 	}
+	return result
+
+	// if operand2 == 1 {
+	// 	return "1+one"
+	// } else if operand2 == 2 {
+	// 	return "1+two"
+	// } else if operand2 == 3 {
+	// 	return "1+three"
+	// } else if operand2 == 4 {
+	// 	return "1+four"
+	// } else if operand2 == 5 {
+	// 	return "1+five"
+	// } else {
+	// 	result = strconv.Itoa(operand2)
+	// 	return "Error " + result
+	// }
 
 }
