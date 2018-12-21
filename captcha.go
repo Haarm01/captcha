d460fd4ddb531a61a164b0bc28c38673baf14bb1
DESKTOP-VSIOK4A\Nok <omerta@outlook.co.th>
2018-12-22 01:08:49 +0700

    refactor test case from hard code many cases to for loop each case

--- captcha.go
+++ captcha.go
@@ -8,64 +8,37 @@ func main() {
 
 func Captcha(pattern, operand1, operator, operand2 int) string {
 
-	// patTern := [2]string{"1", "2"}
-	// opeRand1 := [5]int{1, 2, 3, 4, 5}
 	opeRator := [3]string{"+", "-", "*"}
 	opeRand := [9]string{"one", "two", "three", "four", "five", "six", "seven", "eigth", "nine"}
-
-	op1 := ""
-	oper := ""
-	op2 := ""
+	opd1 := ""
+	opt := ""
+	opd2 := ""
 	result := ""
 
-	// for i, v := range opeRand1 {
-	// 	if i+1 == operand1 {
-	// 		op1 = strconv.Itoa(v)
-	// 	}
-	// }
 	if pattern <= 2 && pattern >= 1 && operand1 <= 9 && operand1 >= 1 && operator <= 3 && operator >= 1 && operand2 <= 9 && operand2 >= 1 {
-
 		for i, v := range opeRator {
 			if i+1 == operator {
-				oper = v
+				opt = v
 			}
 		}
-
 		for i, v := range opeRand {
 			if i+1 == operand1 {
 				if pattern == 1 {
-					op1 = strconv.Itoa(i + 1)
+					opd1 = strconv.Itoa(i + 1)
 				} else {
-					op1 = v
+					opd1 = v
 				}
 			}
 			if i+1 == operand2 {
 				if pattern == 1 {
-					op2 = v
+					opd2 = v
 				} else {
-					op2 = strconv.Itoa(i + 1)
+					opd2 = strconv.Itoa(i + 1)
 				}
 			}
 		}
 	}
-
-	result = op1 + oper + op2
-
+	result = opd1 + opt + opd2
 	return result
-
-	// if operand2 == 1 {
-	// 	return "1+one"
-	// } else if operand2 == 2 {
-	// 	return "1+two"
-	// } else if operand2 == 3 {
-	// 	return "1+three"
-	// } else if operand2 == 4 {
-	// 	return "1+four"
-	// } else if operand2 == 5 {
-	// 	return "1+five"
-	// } else {
-	// 	result = strconv.Itoa(operand2)
-	// 	return "Error " + result
-	// }
 
 }
--- captcha_test.go
+++ captcha_test.go
@@ -1,58 +1,40 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"strconv"
+	"testing"
+)
 
 func TestCaptcha1111To1115Say1PlusOneToFive(t *testing.T) {
 
-	for i := 1; i <= 5; i++ {
-		value := ""
-		if i == 1 {
-			value = "1+one"
-		} else if i == 2 {
-			value = "1+two"
-		} else if i == 3 {
-			value = "1+three"
-		} else if i == 4 {
-			value = "1+four"
-		} else if i == 5 {
-			value = "1+five"
-		}
-		result := Captcha(1, 1, 1, i)
-		expected := value
-		if result != expected {
-			t.Errorf(result)
+	opeRand := [9]string{"one", "two", "three", "four", "five", "six", "seven", "eigth", "nine"}
+	opeRator := [3]string{"+", "-", "*"}
+	opd1 := ""
+	opt := ""
+	opd2 := ""
+
+	for pt := 1; pt <= 2; pt++ {
+		for iopd1, vopd1 := range opeRand {
+			for ioperator, voperator := range opeRator {
+				for iopd2, vopd2 := range opeRand {
+					if pt == 1 {
+						opd1 = strconv.Itoa(iopd1 + 1)
+						opd2 = vopd2
+					} else {
+						opd1 = vopd1
+						opd2 = strconv.Itoa(iopd2 + 1)
+					}
+					opt = voperator
+					result := Captcha(pt, iopd1+1, ioperator+1, iopd2+1)
+					expected := opd1 + opt + opd2
+					fmt.Println(result)
+					fmt.Println(expected)
+					if result != expected {
+						t.Errorf(result)
+					}
+				}
+			}
 		}
 	}
 }
-
-// func TestCaptcha1112Say1PlusTwo(t *testing.T) {
-// 	result := Captcha(1, 1, 1, 2)
-// 	expected := "1+two"
-// 	if result != expected {
-// 		t.Errorf(result)
-// 	}
-// }
-
-// func TestCaptcha1113Say1PlusThree(t *testing.T) {
-// 	result := Captcha(1, 1, 1, 3)
-// 	expected := "1+three"
-// 	if result != expected {
-// 		t.Errorf(result)
-// 	}
-// }
-
-// func TestCaptcha1114Say1PlusFour(t *testing.T) {
-// 	result := Captcha(1, 1, 1, 4)
-// 	expected := "1+four"
-// 	if result != expected {
-// 		t.Errorf(result)
-// 	}
-// }
-
-// func TestCaptcha1115Say1PlusFive(t *testing.T) {
-// 	result := Captcha(1, 1, 1, 5)
-// 	expected := "1+five"
-// 	if result != expected {
-// 		t.Errorf(result)
-// 	}
-// }
